517418b4b0000b4e414fd89c78b438ac49e8d8be
James Clark <jamescl@outlook.com>
2020-12-24 01:40:53 -0800

    Initial commit

---
+++ Configuration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+
+namespace MSCognitiveTextToSpeech
+{
+    /// <summary>
+    /// Handles getting settings from the dll.config file
+    /// </summary>
+    public class Configuration
+    {
+        private AppSettingsSection appSettings;
+        private NumberFormatInfo numberFormats;
+
+        public Configuration()
+        {
+            UriBuilder uri = new UriBuilder(Assembly.GetExecutingAssembly().CodeBase);
+            Path = Uri.UnescapeDataString(uri.Path);
+
+            System.Configuration.Configuration configFile = ConfigurationManager.OpenExeConfiguration(Path);
+
+            appSettings = configFile.AppSettings;
+
+            SettingCount = appSettings.Settings.Count;
+
+            numberFormats = new NumberFormatInfo()
+            {
+                NumberGroupSeparator = "",
+                CurrencyDecimalSeparator = "."
+            };
+        }
+
+        public string Path { get; }
+        public int SettingCount { get; }
+        public bool Exists()
+        {
+            return File.Exists(Path);
+        }
+        public T Setting<T>(string name)
+        {
+            try
+            {
+                return (T)Convert.ChangeType(appSettings.Settings[name].Value, typeof(T), numberFormats);
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+        }
+    }
+}
---
+++ Equalizer.cs
@@ -0,0 +1,87 @@
+﻿//=================================================================================================================
+// AUTHOR: Mark Heath
+// SOURCE: https://github.com/naudio/NAudio/blob/master/NAudioWpfDemo/EqualizationDemo/Equalizer.cs
+// Licensed under the MS-PL license. See LICENSE.md file in the project root for full license information.
+//=================================================================================================================
+
+using NAudio.Dsp;
+using NAudio.Wave;
+
+namespace NAudio
+{
+    class EqualizerBand
+    {
+        public float Frequency { get; set; }
+        public float Gain { get; set; }
+        public float Bandwidth { get; set; }
+    }
+
+    /// <summary>
+    /// Basic multi-band eq using the same settings for both channels in stereo audio
+    /// Call Update after you've updated the bands
+    /// </summary>
+    class Equalizer : ISampleProvider
+    {
+        private readonly ISampleProvider sourceProvider;
+        private readonly EqualizerBand[] bands;
+        private readonly BiQuadFilter[,] filters;
+        private readonly int channels;
+        private readonly int bandCount;
+        private bool updated;
+
+        public Equalizer(ISampleProvider sourceProvider, EqualizerBand[] bands)
+        {
+            this.sourceProvider = sourceProvider;
+            this.bands = bands;
+            channels = sourceProvider.WaveFormat.Channels;
+            bandCount = bands.Length;
+            filters = new BiQuadFilter[channels, bands.Length];
+            CreateFilters();
+        }
+
+        private void CreateFilters()
+        {
+            for (int bandIndex = 0; bandIndex < bandCount; bandIndex++)
+            {
+                var band = bands[bandIndex];
+                for (int n = 0; n < channels; n++)
+                {
+                    if (filters[n, bandIndex] == null)
+                        filters[n, bandIndex] = BiQuadFilter.PeakingEQ(sourceProvider.WaveFormat.SampleRate, band.Frequency, band.Bandwidth, band.Gain);
+                    else
+                        filters[n, bandIndex].SetPeakingEq(sourceProvider.WaveFormat.SampleRate, band.Frequency, band.Bandwidth, band.Gain);
+                }
+            }
+        }
+
+        public void Update()
+        {
+            updated = true;
+            CreateFilters();
+        }
+
+        public WaveFormat WaveFormat => sourceProvider.WaveFormat;
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            int samplesRead = sourceProvider.Read(buffer, offset, count);
+
+            if (updated)
+            {
+                CreateFilters();
+                updated = false;
+            }
+
+            for (int n = 0; n < samplesRead; n++)
+            {
+                int ch = n % channels;
+
+                for (int band = 0; band < bandCount; band++)
+                {
+                    buffer[offset + n] = filters[ch, band].Transform(buffer[offset + n]);
+                }
+            }
+            return samplesRead;
+        }
+    }
+}
